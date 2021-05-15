eea11d83cfc9c4da394eb17db6f93083a225ddd7
Andrew Sadovinskiy <sadovinskiy@gmail.com>
2021-05-15 22:08:26 +0300

    Update scss/add html

---
+++ src/components/mainPage/MainPageSetSettings/index.jsx
@@ -0,0 +1,26 @@
+import s from "../index.module.scss"
+import Validator from "./validator"
+function Settings(props) {
+    const { isErrors, setIsErrors } = props;
+    return (
+        <form className={s.FormSet}>
+            <label htmlFor={"times"}>
+            <span>Duration of the game</span>
+                <input id={"times"} type={"number"} onBlur={(e)=>setIsErrors(Validator(e.target.value))}/>
+                {isErrors && <span className={s.Error}>Must be greater than 120</span>}
+            </label>
+            <label htmlFor={"Gtype"}>
+                <span>Game type</span>
+            <select name={"gameStyle"} id={"Gtype"} defaultValue={"default"}>
+                    <optgroup>
+                        <option disabled={true} value="default">Number of cards in the deck</option>
+                        <option value="52">52 cards</option>
+                        <option value="36">36 cards</option>
+                        <option value="32" >32 cards</option>
+                    </optgroup>
+                </select>
+            </label>
+        </form>
+    )
+}
+export default Settings
