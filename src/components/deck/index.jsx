28ef8bce7216b9fff8e264e7a406e96efe218f10
Andrew Sadovinskiy <sadovinskiy@gmail.com>
2021-05-16 13:05:18 +0300

    deck config

--- src/components/deck/index.jsx
+++ src/components/deck/index.jsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 
 
 
-function Deck() {
+function Deck(props) {
+    let {times,Gmode}=props;
+    const deck=new Array(Gmode)
+    useEffect(()=>{
+       times>0? (times-=1):alert("That is all")
+    },[times])
     return(
-        <>
-        </>
+        <main>
+            <h1>{times}</h1>
+        </main>
     )
 }
