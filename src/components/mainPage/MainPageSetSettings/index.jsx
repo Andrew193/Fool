41a784d0cce63fbe3a4949ef32533e6ffb4ea9d0
Andrew Sadovinskiy <sadovinskiy@gmail.com>
2021-05-16 12:41:30 +0300

    fix

--- src/components/mainPage/MainPageSetSettings/index.jsx
+++ src/components/mainPage/MainPageSetSettings/index.jsx
@@ -1,17 +1,17 @@
 import s from "../index.module.scss"
 import Script from "./validator"
 function Settings(props) {
-    const { isErrors, setIsErrors,setDarkMode } = props;
+    const { isErrors, setIsErrors, setDarkMode } = props;
     return (
         <form className={s.FormSet}>
             <label htmlFor={"times"}>
-            <span>Duration of the game</span>
-                <input id={"times"} type={"number"} onBlur={(e)=>Script.Updater(setIsErrors,Script.Validator(e.target.value))}/>
+                <span>Duration of the game</span>
+                <input id={"times"} type={"number"} onBlur={(e) => Script.Updater(setIsErrors, Script.Validator(e.target.value))} />
                 {isErrors && <span className={s.Error}>Must be greater than 120</span>}
             </label>
             <label htmlFor={"Gtype"}>
                 <span>Game type</span>
-            <select name={"gameStyle"} id={"Gtype"} defaultValue={"default"}>
+                <select name={"gameStyle"} id={"Gtype"} defaultValue={"default"}>
                     <optgroup>
                         <option disabled={true} value="default">Number of cards in the deck</option>
                         <option value="52">52 cards</option>
@@ -20,11 +20,11 @@ function Settings(props) {
                     </optgroup>
                 </select>
             </label>
-            <label htmlFor={"gameMode"} className="gameMode">
-            <input type={"radio"} value={"true"} name={"mode"} onClick={(e)=>{setDarkMode(true)}}/>Dark Mode
+            <label htmlFor={"gameMode"} className={s.gameMode}>
+                <input type={"radio"} value={"true"} name={"mode"} onClick={() => { setDarkMode(true) }} /><span>Dark Mode</span>
             </label>
-            <label htmlFor={"gameMode"} className="gameMode">
-            <input type={"radio"} value={"false"} name={"mode"} onClick={(e)=>{setDarkMode(false)}}/>Light Mode
+            <label htmlFor={"gameMode"} className={s.gameMode}>
+                <input type={"radio"} value={"false"} name={"mode"} onClick={() => { setDarkMode(false) }} /><span>Light Mode</span>
             </label>
         </form>
     )
