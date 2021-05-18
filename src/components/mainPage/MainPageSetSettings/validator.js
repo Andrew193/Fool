e88f71b6417baba18a9e20671792106fdd67322e
Andrew Sadovinskiy <sadovinskiy@gmail.com>
2021-05-18 14:37:54 +0300

    Updates

--- src/components/mainPage/MainPageSetSettings/validator.js
+++
@@ -1,9 +0,0 @@
-
-const Validator = (value) => ((value < 120 || !value) ? true : false)
-const Updater = (setter, toSet) => {
-    setter(toSet)
-}
-module.exports= {
-    Validator: Validator,
-    Updater: Updater
-};
